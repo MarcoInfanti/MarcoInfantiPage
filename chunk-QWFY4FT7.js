c85d26e5681cf78bd3268f8e595e08af2fd6352b
MarcoInfanti <marcoinfanti1994@gmail.com>
2026-05-13 18:31:16 +0200

    Auto-generated commit

--- chunk-QWFY4FT7.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-7CGTOI24.js";var o=[{path:"table",loadComponent:()=>import("./chunk-Y4VVYGV3.js").then(e=>e.TableDemoComponent)}],t=o;export{o as DEMOS_ROUTES,t as default};
---
+++ chunk-QZOABJ4C.js
@@ -0,0 +1,1 @@
+import"./chunk-7CGTOI24.js";var o=[{path:"table",loadComponent:()=>import("./chunk-S3NXDP6Y.js").then(e=>e.TableDemoComponent)}],t=o;export{o as DEMOS_ROUTES,t as default};
